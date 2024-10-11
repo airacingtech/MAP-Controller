7722ae888bd6f84939e94a2ec407eb7fe101e9f2
Adith1 <adithsundram@gmail.com>
2024-10-10 18:42:07 -0700

    changed to simplified AV24 dynamics, working on adding bank and curv to LUT

--- steering_lookup/src/LUT_Generation/dynamics/vehicle_dynamics_stown.py
+++ steering_lookup/src/LUT_Generation/dynamics/vehicle_dynamics_stown.py
@@ -1,5 +1,6 @@
 import math
-from re import A
+import math
+import casadi as ca
 
 __author__ = "Matthias Althoff"
 __copyright__ = "TUM Cyber-Physical Systems Group"
@@ -112,6 +113,7 @@ def vehicle_dynamics_st_delayed(x, uInit, p, type):
 
     return f
 
+
 def vehicle_dynamics_st(x, uInit, p, type):
     """
     vehicleDynamics_st - single-track vehicle dynamics
@@ -127,77 +129,100 @@ def vehicle_dynamics_st(x, uInit, p, type):
 
     Outputs:
         :return f: right-hand side of differential equations
-
-    Author: Matthias Althoff
-    Written: 12-January-2017
-    Last update: 16-December-2017
-                    03-September-2019
-    Last revision: 17-November-2020
     """
 
-    #------------- BEGIN CODE --------------
-
     # set gravity constant
-    g = 9.81  #[m/s^2]
+    g = 9.81  # [m/s^2]
 
-    #create equivalent bicycle parameters
+    # Retrieve parameters
     mu = p.mu
-    if type == "pacejka":
-        B_f = p.C_Pf[0]
-        C_f = p.C_Pf[1]
-        D_f = p.C_Pf[2]
-        E_f = p.C_Pf[3]
-        B_r = p.C_Pr[0]
-        C_r = p.C_Pr[1]
-        D_r = p.C_Pr[2]
-        E_r = p.C_Pr[3]
-    elif type == "linear":
-        C_Sf = p.C_Sf #-p.tire.p_ky1/p.tire.p_dy1  
-        C_Sr = p.C_Sr #-p.tire.p_ky1/p.tire.p_dy1  
     lf = p.l_f
     lr = p.l_r
-    h = p.h_cg 
-    m = p.m 
+    h = p.h_cg
+    m = p.m
     I = p.I_z
+    # twf = p.twf
+    # twr = p.twr
+    # cl_f = p.cl_f
+    # cl_r = p.cl_r
+    # rho = p.rho
+    # A = p.A
+    # kroll_f = p.kroll_f
+    ax = uInit[1]  # longitudinal acceleration
+    delta = uInit[0]  # steering angle
+    bank = uInit[2]  # bank angle
+    k = uInit[3]
 
-    #states
-    #x0 = x-position in a global coordinate system
-    #x1 = y-position in a global coordinate system
-    #x2 = yaw angle
-    #x3 = velocity in x-direction
-    #x4 = velocity in y direction
-    #x5 = yaw rate
+    Bf = p.f_pacejka_b
+    Cf = p.f_pacejka_c
+    Ef = p.f_pacejka_e
+    Br = p.f_pacejka_b
+    Cr = p.r_pacejka_c
+    Er = p.r_pacejka_e
 
-    #u1 = steering angle
-    #u2 = longitudinal acceleration
+    # States
+    vx = x[3]
+    vy = x[4]
+    omega = x[5]  # yaw rate
+    
+    # Compute normal force N with bank angle consideration
+    N = m * g * ca.cos(bank) - (m * (vx**2) * k) * ca.sin(bank)
 
-    u = uInit
+    # Vertical tire forces
+    # Fz_f = 0.5 * N * lr / (lf + lr) - 0.5 * h / (lf + lr) * m * ax + 0.25 * cl_f * rho * A * vx ** 2
+    # Fz_r = 0.5 * N * lr / (lf + lr) + 0.5 * h / (lf + lr) * m * ax + 0.25 * cl_r * rho * A * vx ** 2
+    # gamma_y = vy / vx  # body roll effect
 
-    # system dynamics
-
-    # compute lateral tire slip angles
-    alpha_f = -math.atan((x[4] + x[5] * lf) / x[3]) + u[0] 
-    alpha_r = -math.atan((x[4] - x[5] * lr) / x[3])
+    Fz_f = m * (-ax * h + g * lr) / (lr + lf)
+    Fz_r = m * (ax * h + g * lf) / (lr + lf)
 
 
-    # compute vertical tire forces
-    F_zf = m * (-u[1] * h + g * lr) / (lr + lf)
-    F_zr = m * (u[1] * h + g * lf) / (lr + lf)
+    # Fz_fl = Fz_f - kroll_f * gamma_y
+    # Fz_fr = Fz_f + kroll_f * gamma_y
+    # Fz_rl = Fz_r - (1 - kroll_f) * gamma_y
+    # Fz_rr = Fz_r + (1 - kroll_f) * gamma_y
 
-    F_yf = F_yr = 0
+    # Compute lateral tire slip angles
+    # a_fl = -ca.atan2((vy + lf * omega) * ca.cos(delta) - vx * ca.sin(delta),
+    #                  vx * ca.cos(delta) + (vy + lf * omega) * ca.sin(beta) - 0.5 * twf * omega)
+    # a_fr = -ca.atan2((vy + lf * omega) * ca.cos(delta) - vx * ca.sin(delta),
+    #                  vx * ca.cos(delta) + (vy + lf * omega) * ca.sin(beta) + 0.5 * twf * omega)
+    # a_rl = ca.atan2((lr * omega - vx * ca.sin(beta)),
+    #                 (vx * ca.cos(beta) - 0.5 * twr * omega))
+    # a_rr = ca.atan2((lr * omega - vx * ca.sin(beta)),
+    #                 (vx * ca.cos(beta) + 0.5 * twr * omega))
 
-    # combined slip lateral forces
+    # Compute lateral tire forces based on simplified dynamics
+    # if type == "pacejka":
+    #     F_yf = mu * Fz_fl * ca.sin(p.Cf * ca.arctan(p.Bf * a_fl))
+    #     F_fr = mu * Fz_fr * ca.sin(p.Cf * ca.arctan(p.Bf * a_fr))
+    #     F_yr = mu * Fz_rl * ca.sin(p.Cr * ca.arctan(p.Br * a_rl))
+    #     F_rr = mu * Fz_rr * ca.sin(p.Cr * ca.arctan(p.Br * a_rr))
+    # elif type == "linear":
+    #     F_yf = mu * Fz_fl * p.C_Sf * a_fl
+    #     F_fr = mu * Fz_fr * p.C_Sf * a_fr
+    #     F_yr = mu * Fz_rl * p.C_Sr * a_rl
+    #     F_rr = mu * Fz_rr * p.C_Sr * a_rr
+
+
+    a_f = -math.atan((x[4] + x[5] * lf) / x[3]) + delta 
+    a_r = -math.atan((x[4] - x[5] * lr) / x[3])
+
     if type == "pacejka":
-        F_yf = mu * F_zf * D_f * math.sin(C_f * math.atan(B_f * alpha_f - E_f*(B_f * alpha_f - math.atan(B_f * alpha_f))))
-        F_yr = mu * F_zr * D_r * math.sin(C_r * math.atan(B_r * alpha_r - E_r*(B_r * alpha_r - math.atan(B_r * alpha_r))))
+        F_yf = mu * Fz_f * ca.sin(Cf * ca.arctan(Bf * a_f))
+        F_fr = mu * Fz_f * ca.sin(Cf * ca.arctan(Bf * a_f))
+        F_yr = mu * Fz_r * ca.sin(Cr * ca.arctan(Br * a_r))
+        F_rr = mu * Fz_r* ca.sin(Cr * ca.arctan(Br * a_r))
     elif type == "linear":
-        F_yf = mu * F_zf * C_Sf * alpha_f
-        F_yr = mu * F_zr * C_Sr * alpha_r
+        F_yf = mu * Fz_f * p.C_Sf * a_f
+        F_fr = mu * Fz_f * p.C_Sf * a_f
+        F_yr = mu * Fz_r * p.C_Sr * a_r
+        F_rr = mu * Fz_r * p.C_Sr * a_r
 
     f = [x[3]*math.cos(x[2]) - x[4]*math.sin(x[2]), 
         x[3]*math.sin(x[2]) + x[4]*math.cos(x[2]), 
         x[5], 
-        u[1], 
+        ax, 
         1/m * (F_yr + F_yf) - x[3] * x[5],
         1/I * (-lr * F_yr + lf * F_yf)] 
         # -mu*m/(x[3]*I*(lr+lf))*(lf**2*C_Sf*(g*lr-u[1]*h) + lr**2*C_Sr*(g*lf + u[1]*h))*x[5] \
@@ -210,6 +235,17 @@ def vehicle_dynamics_st(x, uInit, p, type):
         # -x[5] + 1 / (m * x[3]) * (F_yf * math.cos(u[0] - x[6]) \
         #     + F_yr * math.cos(x[6]))]
         
+
+    # # System dynamics (same structure as before)
+    # f = [vx * ca.cos(beta + x[4]),
+    #      vx * ca.sin(beta + x[4]),
+    #      0,  # Placeholder for unused dynamic (e.g., z)
+    #      uInit[1],  # Longitudinal acceleration
+    #      x[5]]  # Yaw rate
+
+    # # Yaw and lateral dynamics update
+    # f.append(1 / I * (F_yf * ca.cos(delta) * lf - F_yr * lr))
+    # f.append(-x[5] + 1 / (m * vx) * (F_yf * ca.cos(delta - beta) + F_yr * ca.cos(beta)))
 
     return f
 
--- steering_lookup/src/LUT_Generation/simulate_model.py
+++ steering_lookup/src/LUT_Generation/simulate_model.py
@@ -3,65 +3,106 @@ from helpers.load_model import get_dotdict
 from scipy.integrate import odeint
 import numpy as np
 
-model_name = "NUC4_pacejka"
+model_name = "AV24_pacejka"
 model, tiretype = model_name.split("_")
 
 model = get_dotdict(model_name)
 
 class Simulator:
-  def func_ST(self, x, t, u, p):
-      f = vehicle_dynamics_st(x, u, p, tiretype)
-      return f
+    def func_ST(self, x, t, u, p):
+        f = vehicle_dynamics_st(x, u, p, tiretype)
+        return f
 
-  def generate_lookup(self):
-    start_steer = 0.0
-    steer_fine_end = 0.1
-    end_steer = 0.4
-    n_steps_steer = 30 # used for fine region and then again for coarse region
-    start_vel = 0.5
-    end_vel = 7.0
-    n_steps_vel = 65
-    
-    fine_steers = np.linspace(start_steer, steer_fine_end, n_steps_steer)
-    steers = np.linspace(steer_fine_end, end_steer, n_steps_steer)
-    steers = np.concatenate((fine_steers, steers))
-    vels = np.linspace(start_vel, end_vel, n_steps_vel)
+    def generate_lookup(self):
+        start_steer = 0.0
+        steer_fine_end = 0.1
+        end_steer = 0.1
+        n_steps_steer = 30  # used for fine region and then again for coarse region
+        start_vel = 0.5
+        end_vel = 70.0
+        n_steps_vel = 65
+        start_bank = 0.0  # Range for bank angle in radians (slight negative tilt)
+        end_bank = -0.35  # Slight positive tilt
+        n_steps_bank = 15  # Number of steps for bank angle
+        # end_bank = 0.0
+        # n_steps_bank = 1
+        start_curv = 0.0  # Range for bank angle in radians (slight negative tilt)
+        # end_curv = 0.0
+        # n_steps_curv = 1
+        end_curv = 0.006  # Slight positive tilt
+        n_steps_curv = 10  # Number of steps for curv
 
-    n_steps_steer = len(steers)
-    self.lookup_table = np.empty([n_steps_steer + 1, n_steps_vel +1])    
+        fine_steers = np.linspace(start_steer, steer_fine_end, n_steps_steer)
+        steers = np.linspace(steer_fine_end, end_steer, n_steps_steer)
+        steers = np.concatenate((fine_steers, steers))
+        vels = np.linspace(start_vel, end_vel, n_steps_vel)
+        banks = np.linspace(start_bank, end_bank, n_steps_bank)
+        curvatures = np.linspace(start_curv, end_curv, n_steps_curv)
 
-    self.lookup_table[0, 1:] = vels
-    self.lookup_table[1:, 0] = steers
+        n_steps_steer = len(steers)
+        n_steps_bank = len(banks)
+        self.lookup_table = np.empty([n_steps_steer + 1, n_steps_vel + 1, n_steps_bank + 1, n_steps_curv +1])
 
-    for steer_idx, steer in enumerate(steers):
-      for vel_idx, vel in enumerate(vels):
-        initialState = [0, 0, 0, vel, 0, 0]
-        u = [steer, 0]
-        
-        # print("Steering Angle :" + str(steer) + " Velocity :" + str(vel))
-        dt = 0.01
-        duration = 2.0
-        t = np.arange(0, duration, dt)
+        self.lookup_table[0, 1:, 0,0] = vels  # Velocity row
+        self.lookup_table[1:, 0, 0,0] = steers  # Steering column
+        self.lookup_table[0, 0, 1:,0] = banks  # Bank angle row
+        self.lookup_table[0, 0, 0,1:] = curvatures  # Bank angle row
 
-        self.sol = odeint(self.func_ST, initialState, t, args=(u, model))  
 
-        if abs(self.sol[-1, 5] - self.sol[-10, 5]) > 0.05:
-          #print("failed to converge, limit")
-          self.lookup_table[steer_idx+1, vel_idx+1:] = None
-          break
+        for steer_idx, steer in enumerate(steers):
+            for vel_idx, vel in enumerate(vels):
+                for bank_idx, bank in enumerate(banks):
+                    for k_idx, k in enumerate(curvatures):
+                      initialState = [0, 0, 0, vel, 0, 0]
+                      u = [steer, 0, bank, k]  # Now includes bank as part of input
+                      
+                      dt = 0.01
+                      duration = 2.0
+                      t = np.arange(0, duration, dt)
 
-        else:
-          a_lat = self.sol[-1, 5] * vel
-          a_lat_all = self.sol[:, 5] * vel
-          self.lookup_table[steer_idx+1, vel_idx+1] = a_lat
+                      self.sol = odeint(self.func_ST, initialState, t, args=(u, model))
+                      # print(steer)
+                      # print(vel)
+                      # print(bank)
+                      # print(k)
+                      if abs(self.sol[-1, 5] - self.sol[-10, 5]) > 0.05:
+                          # print("failed to converge, limit")
+                          self.lookup_table[steer_idx+1, vel_idx+1, bank_idx+1,k_idx +1] = None
+                          break
 
-  def save_lookup(self):
-    model, tires = model_name.split("_")
-    file_path = "./models/" + model + "/" + model_name + "_lookup_table.csv"
-    np.savetxt(file_path, self.lookup_table, delimiter=",")
+                      else:
+                          a_lat = self.sol[-1, 5] * vel
+                          a_lat_all = self.sol[:, 5] * vel
+                          # print("saved")
+
+                          self.lookup_table[steer_idx+1, vel_idx+1, bank_idx+1, k_idx+1] = a_lat
+
+    def save_lookup(self):
+      model, tires = model_name.split("_")
+      file_path = "./models/" + model + "/" + model_name + "_lookup_table.csv"
+      
+      # Flatten the arrays for steer, vel, bank, curv, and lateral_acc
+      steer_vals, vel_vals, bank_vals, curv_vals, lateral_acc_vals = [], [], [], [], []
+      
+      for steer_idx, steer in enumerate(self.lookup_table[1:, 0, 0, 0]):
+          for vel_idx, vel in enumerate(self.lookup_table[0, 1:, 0, 0]):
+              for bank_idx, bank in enumerate(self.lookup_table[0, 0, 1:, 0]):
+                  for curv_idx, curv in enumerate(self.lookup_table[0, 0, 0, 1:]):
+                      lateral_acc = self.lookup_table[steer_idx+1, vel_idx+1, bank_idx+1, curv_idx+1]
+                      steer_vals.append(steer)
+                      vel_vals.append(vel)
+                      bank_vals.append(bank)
+                      curv_vals.append(curv)
+                      lateral_acc_vals.append(lateral_acc)
+
+      # Stack the columns together
+      data = np.column_stack((steer_vals, vel_vals, bank_vals, curv_vals, lateral_acc_vals))
+      
+      # Save to CSV
+      np.savetxt(file_path, data, delimiter=",", header="steer,vel,bank,curv,lateral_acc", comments="")
 
 if __name__ == "__main__":
-  sim = Simulator()
-  sim.generate_lookup()
-  sim.save_lookup()
-  print("Done")
+    sim = Simulator()
+    sim.generate_lookup()
+    sim.save_lookup()
+    print("Done")
